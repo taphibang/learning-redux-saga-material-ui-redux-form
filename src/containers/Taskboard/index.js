4df0af5d315af8b771f4e3173156eb323d7a5fbc
Tạ Phi Bằng <taphibang@gmail.com>
2020-06-16 10:49:39 +0700

    update

--- src/containers/Taskboard/index.js
+++ src/containers/Taskboard/index.js
@@ -5,38 +5,98 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Grid from '@material-ui/core/Grid';
 import { STATUSES } from '../../constanst';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const listTask = [
+   {
+      id: 1,
+      title: "Read Hook",
+      description: "Read material ui book",
+      status: 0
+   },
+   {
+      id: 2,
+      title: "Play Game",
+      description: "Read Game",
+      status: 2
+   },
+   {
+      id: 3,
+      title: "Play Bowling",
+      description: "",
+      status: 1
+   }
+];
 
 class TaskBoard extends Component {
 
-    renderBoard(){
-        let xhtml = null;
-        xhtml = (
-            <Grid container spacing={3}>
-                {
-                    STATUSES.map((status, index) => {
-                        return (
-                            <Grid item md={4} xs={12} key={status.value}>
-                                {status.label}
-                            </Grid>
-                        );
-                    })
-                }
-            </Grid>
-        );
-        return xhtml;
-    }
+   renderBoard() {
+      const { classes } = this.props;
+      let xhtml = null;
+      xhtml = (
+         <Grid container spacing={3}>
+            {
+               STATUSES.map((status, index) => {
+                  const taskFiltered = listTask.filter(task => task.status === status.value);
+                  return (
+                     <Grid item md={4} xs={12} key={status.value}>
+                        <Box mt={2} mb={2}>
+                           <div className={classes.status}>
+                              {status.label}
+                           </div>
+                        </Box>
+                        <div className={classes.wrapperListTask}>
+                           {
+                              taskFiltered.map(task => {
+                                 const {title} = task;
+                                 return (
+                                    <Card key={task.id} className={classes.card}>
+                                       <CardContent>
+                                          <Grid container justify="space-between">
+                                             <Grid item md={8}>
+                                                <Typography component="h2">
+                                                   {title}
+                                                </Typography>
+                                             </Grid>
+                                             <Grid item md={4}>
+                                                {
+                                                   status.label
+                                                }
+                                             </Grid>
+                                          </Grid>
+                                       </CardContent>
+                                       <CardActions>
+                                          <Button size="small"></Button>
+                                       </CardActions>
+                                    </Card>
+                                 )
+                              })
+                           }
+                        </div>
+                     </Grid>
+                  );
+               })
+            }
+         </Grid>
+      );
+      return xhtml;
+   }
 
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className={classes.taskBoard}>
-                <Button variant="contained" color="primary" className={classes.button}>
-                    <AddIcon/> Thêm Công Việc
+   render() {
+      const { classes } = this.props;
+      return (
+         <div className={classes.taskBoard}>
+            <Button variant="contained" color="primary" className={classes.button}>
+               <AddIcon /> Thêm Công Việc
                 </Button>
-                {this.renderBoard()}
-            </div>
-        );
-    }
+            {this.renderBoard()}
+         </div>
+      );
+   }
 }
 
 export default withStyles(styles)(TaskBoard);
