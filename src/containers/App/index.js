af6d02cc02b037860b47f9dc71877740e024fa27
Tạ Phi Bằng <taphibang@gmail.com>
2020-06-16 09:28:49 +0700

    update first

---
+++ src/containers/App/index.js
@@ -0,0 +1,18 @@
+import styles from './styles';
+import {withStyles} from '@material-ui/core';
+import {ThemeProvider } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import Taskboard from '../Taskboard/index.js';
+import theme from '../commons/Theme';
+
+class App extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Taskboard/>
+            </ThemeProvider>
+        );
+    }
+}
+
+export default withStyles(styles)(App);
---
+++ src/containers/commons/Theme/index.js
@@ -0,0 +1,20 @@
+import { createMuiTheme} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    color : {
+        primary : "#ff784e",
+        secondary : "#ffa733",
+        error : "#e57373"
+    },
+    typography : {
+        fontFamily: "Roboto"
+    },
+    shape : {
+        borderRadius : 4,
+        backgroundColor : "#388e3c",
+        textColor : "#FFFFFF",
+        border : "#CCCCCC"
+    }
+});
+
+export default theme;
