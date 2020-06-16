ad2eefa4633e3c51cd12a44a9371bad6d8ceb48d
Tạ Phi Bằng <taphibang@gmail.com>
2020-06-16 10:19:10 +0700

    update

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,7 +3,7 @@ import {withStyles} from '@material-ui/core';
 import {ThemeProvider } from '@material-ui/core/styles';
 import React, { Component } from 'react';
 import Taskboard from '../Taskboard/index.js';
-import theme from '../commons/Theme';
+import theme from '../../commons/Theme';
 
 class App extends Component {
     render() {
--- src/containers/Taskboard/index.js
+++ src/containers/Taskboard/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core';
 import styles from './styles';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Grid from '@material-ui/core/Grid';
 
 class TaskBoard extends Component {
+
+    renderBoard(){
+        let xhtml = null;
+        xhtml = (
+            <Grid container spacing={3}>
+                <Grid item md={4} xs={12}>
+                    READY
+                </Grid>
+                <Grid item md={4} xs={12}>
+                    PROCESSING
+                </Grid>
+                <Grid item md={4} xs={12}>
+                    COMPLETE
+                </Grid>
+            </Grid>
+        );
+        return xhtml;
+    }
+
     render() {
-        const {classes} = this.props;
+        const { classes } = this.props;
         return (
-            <div className={classes.taskboard}>
-                <div className={classes.shape}>
-                    React JS
-                </div>
-                <div className={classes.shape}>
-                    Angular JS
-                </div>
+            <div className={classes.taskBoard}>
+                <Button variant="contained" color="primary" className={classes.button}>
+                    <AddIcon/> Thêm Công Việc
+                </Button>
+                {this.renderBoard()}
             </div>
         );
     }
