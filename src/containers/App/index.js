a6a442b5c84594bcac82d4aaae851943834e3412
Tạ Phi Bằng <taphibang@gmail.com>
2020-07-01 10:47:30 +0700

    update

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,22 @@
-import styles from './styles';
 import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 import React, { Component } from 'react';
-import Taskboard from '../Taskboard/index.js';
+import { Provider } from 'react-redux';
 import theme from '../../commons/Theme';
+import configureStore from '../../redux/configureStore';
+import Taskboard from '../Taskboard/index.js';
+import styles from './styles';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Taskboard />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Taskboard />
+        </ThemeProvider>
+      </Provider>
     );
   }
 }
